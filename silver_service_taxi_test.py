090f2fe5b118be69216daa58b397e0f9b55ea88e
hoangtuanvp2011 <t00aienl@gmail.com>
2021-01-07 22:37:16 +0700

    create object Hummer

---
+++ silver_service_taxi_test.py
@@ -0,0 +1,9 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    Hummer = SilverServiceTaxi("Hummer", 200)
+    print(Hummer.__str__())
+
+if __name__ == '__main__':
+    main()
