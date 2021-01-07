481f1c538981241f13ff2bc6bbf7a439ca6e4247
hoangtuanvp2011 <t00aienl@gmail.com>
2021-01-07 22:43:53 +0700

    add fanciness attribute

--- silver_service_taxi.py
+++ silver_service_taxi.py
@@ -3,10 +3,9 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
-    def __init__(self, name, fuel=0):
+    def __init__(self, name, fuel=0, fanciness=1):
         super().__init__(name, fuel)
-        self.fanciness = 2
-        self.price_per_km = super().get_price_per_km() * 4
+        self.price_per_km = super().get_price_per_km() * fanciness
         self.flagfall = 4.5
 
     def drive(self, distance):
--- silver_service_taxi_test.py
+++ silver_service_taxi_test.py
@@ -2,7 +2,7 @@ from silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    Hummer = SilverServiceTaxi("Hummer", 200)
+    Hummer = SilverServiceTaxi("Hummer", 200, 4)
     print(Hummer.__str__())
 
 if __name__ == '__main__':
