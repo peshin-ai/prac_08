d05aeb604a00fa933389e15d8a849fd0ed3eb9ff
hoangtuanvp2011 <t00aienl@gmail.com>
2021-01-07 22:18:51 +0700

    set drive 60 and 100 as initial

--- taxi_test.py
+++ taxi_test.py
@@ -4,10 +4,9 @@ from taxi import Taxi
 def main():
 
     Prius_1 = Taxi("Prius 1", 100)
-    Prius_1.drive(40)
+    Prius_1.drive(60)
     print(Prius_1.__str__())
     print(Prius_1.get_fare())
-    Prius_1.set_meter()
     Prius_1.drive(100)
     print(Prius_1.__str__())
     print(Prius_1.get_fare())
